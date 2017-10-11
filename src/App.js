3a089467d3ca0ca58d9429c8a4de4e87e619dd04
ayACEP <srx1982@gmail.com>
2017-10-11 22:58:34 +0800

    map screen partly done

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import H1Sub from './component/H1Sub'
 import Callout from './component/Callout'
 import colors from './colors'
 import css from './styles.css'
-import svg from './svg/logo.svg'
+import mission from './svg/mission.svg'
 import s from './svg/hline.svg'
 
 const styles = {
     button: {
-        flexGrow: '1',
+        flex: '1',
         marginLeft: '24px',
     }
 }
@@ -19,22 +19,49 @@ const styles = {
 class App extends Component {
     
     render() {
-        return <div className='flex-column' style={{padding: '0px 32px'}}>
+        return <div className='flex-column' style={{padding: '0px 32px', color: colors.dark}}>
             <div className='flex-row' style={{
                 padding: '16px 0px 12px 0px'}}>
                 <Callout />
-                <div className='flex-row' style={{flexGrow: '1', justifyContent: 'space-between'}}>
-                    <Button text='MAP' img={svg} style={styles.button} />
-                    <Button text='QUEST' img={svg} style={styles.button} />
-                    <Button text='ITEM' img={svg} style={styles.button} />
-                    <Button text='WEAPON' img={s} style={styles.button} />
+                <div className='flex-row' style={{flex: '1', justifyContent: 'space-between'}}>
+                    <Button text='MAP' img='' style={styles.button} />
+                    <Button text='QUEST' img='' style={styles.button} />
+                    <Button text='ITEM' img='' style={styles.button} />
+                    <Button text='WEAPON' img='' style={styles.button} />
                 </div>
             </div>
             <HLine style={{margin: '0px -32px'}} />
-            <div className='flex-row' style={{alignItems: 'baseline'}}>
-                <H1 style={{margin: '6px 0px 20px'}} text='ITEM' />
-                <H1Sub text='&nbsp;-&nbsp;所有道具' />
+            <div className='flex-row' style={{margin: '10px 0px 36px', alignItems: 'baseline'}}>
+                <H1 text='MAP' />
             </div>
+            <div className='flex-row'>
+                <div className='flex-column' style={{flex: 3, justifyContent: 'space-between', paddingRight: '56px'}}>
+                    <div className='flex-row'>
+                        <Callout />
+                        <div className='flex-column' style={{flex: 1, marginLeft: '24px'}}>
+                            <Button type='small' text='快速保存' img='' />
+                            <Button type='small' text='地图阅览模式' img='' style={{marginTop: '20px'}} />
+                            <Button type='small' text='地图图示一览' img='' style={{marginTop: '20px'}} />
+                        </div>
+                    </div>
+                    <div className='flex-column' style={{letterSpacing: '2px'}}>
+                        <span style={{fontSize: '12px'}}>森林地带</span>
+                        <div className='flex-row' style={{
+                            alignItems: 'center',
+                            borderWidth: '1px 0px', 
+                            borderStyle: 'solid', 
+                            borderColor: colors.grayLight,
+                            margin: '2px 0px 2px 0px',
+                            padding: '6px 4px 6px 4px'}}>
+                            <img width='16px' height='16px' src={mission} alt='' />
+                            <span style={{fontSize: '18px', marginLeft: '8px'}}>飞弹的补给作战</span>
+                        </div>
+                        <span style={{fontSize: '14px', textIndent: '2em'}}>支援沉默都市的飞弹补给作战</span>
+                    </div>
+                </div>
+                <img style={{flex: 7, height: '400px', border: `1px solid ${colors.gray}`}} src='' alt='' />
+            </div>
+            <HLine style={{margin: '32px -32px'}} />
         </div>
     }
 
--- src/component/Button.js
+++ src/component/Button.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react'
 import colors from '../colors'
 
-const style ={
-    img: {
-        width: '18px',
-        height: '18px',
-        marginRight: '4px',
-        backgroundColor: colors.dark
-    }
-}
-
 class Button extends Component {
 
     render() {
+        const small = this.props.type === 'small'
+        const imgWidth = small ? '16px' : '18px'
+        const imgStyle = {
+            width: imgWidth,
+            height: imgWidth,
+            marginRight: '5px',
+            backgroundColor: colors.dark
+        }
+        const textStyle = {
+            color: colors.dark,
+            fontSize: small ? '14px' : '16px'
+        }
         return <div className='flex-row' style={Object.assign({
+            height: '28px',
             paddingLeft: '5px',
-            paddingTop: '3px',
-            paddingBottom: '3px',
             alignItems: 'center', 
             backgroundColor: colors.grayLight}, this.props.style)}>
             {this.props.img != null 
-                && <img style={style.img} src={this.props.img} alt='' />}
-            <span style={{color: colors.dark}}>{this.props.text}</span>
+                && <img style={imgStyle} src={this.props.img} alt='' />}
+            <span style={textStyle}>{this.props.text}</span>
         </div>
     }
 }
